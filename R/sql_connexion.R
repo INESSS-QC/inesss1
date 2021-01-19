6e6c8326b051c2013f48694e96612589e2ab206d
Guillaume Boucher <guiboucher8@gmail.com>
2021-01-19 10:51:26 -0500

    Update sql_connexion.R

--- R/sql_connexion.R
+++ R/sql_connexion.R
@@ -11,29 +11,29 @@
 #'
 #' @encoding UTF-8
 #' @return Connexion Teradata, sinon `NULL`.
-#' @importFrom odbc odbc dbConnect
-#' @importFrom testthat capture_error
-#' @importFrom askpass askpass
 #' @export
 #' @examples
 #' \dontrun{
-#' conn <- sql_connexion("abc007")
-#' conn <- sql_connexion(uid = "abc007", pwd = "MonMotDePasse",
-#'                       dsn = "PEI_PRD", encoding = "latin1")
+#' conn <- SQL_connexion("abc007")
+#' conn <- SQL_connexion(uid = "abc007", pwd = "MonMotDePasse", dsn = "PEI_PRD")
 #' }
 SQL_connexion <- function(uid, pwd = NULL, dsn = "PEI_PRD", encoding = "latin1") {
 
-  if (is.null(pwd)) {  # demander le mot de passe s'il n'est pas inscrit
-    pwd <- askpass("Mot de passe")
+  ### Demander le mot de passe s'il n'est pas inscrit
+  if (is.null(pwd)) {
+    pwd <- askpass::askpass("Mot de passe")
   }
 
-  is_error <- capture_error({  # effectuer la connexion et conserver message erreur s'il y en a
-    sql_conn <- dbConnect(odbc(), dsn, uid = uid, pwd = pwd, encoding = encoding)
+  ### Effectuer la connexion et conserver message erreur s'il y en a
+  is_error <- testthat::capture_error({
+    sql_conn <- odbc::dbConnect(odbc::odbc(), dsn, uid = uid, pwd = pwd, encoding = encoding)
   })
 
-  if (is.null(is_error)) {  # pas d'erreur de connexion
+  ### Connexion, sinon NULL si erreur
+  if (is.null(is_error)) {
     return(sql_conn)
   } else {
     return(NULL)
   }
+
 }
