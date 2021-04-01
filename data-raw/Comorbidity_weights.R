df3722238b5c6069d6b649ddc1e503a30804225c
Guillaume Boucher <guiboucher8@gmail.com>
2021-04-01 13:40:29 -0400

    comorbidity update datas

--- data-raw/Comorbidity_weights.R
+++ data-raw/Comorbidity_weights.R
@@ -2,12 +2,31 @@ library(usethis)
 library(readxl)
 library(data.table)
 
-Comorbidity_weights <- as.data.table(read_excel(
-  "inst/extdata/Comorbidity_weights.xlsx"
-))
-Comorbidity_weights[, `:=` (CIM9 = as.integer(CIM9),
-                            CIM10 = as.integer(CIM10))]
-setkey(Comorbidity_weights, DIAGN_CODE)
+ComorbidityWeights <- list()
 
-attr(Comorbidity_weights, "MaJ") <- Sys.Date()
-use_data(Comorbidity_weights, overwrite = TRUE)
+# CCI_INSPQ_2018_CIM9 -----------------------------------------------------
+
+ComorbidityWeights$CCI_INSPQ_2018_CIM9 <-
+  as.data.table(read_excel("inst/extdata/Comorbidity_weights.xlsx", "CCI_INSPQ_2018_CIM9"))
+ComorbidityWeights$CCI_INSPQ_2018_CIM9[, POIDS := as.integer(POIDS)]
+setkey(ComorbidityWeights$CCI_INSPQ_2018_CIM9, DIAGN_CODE)
+
+
+# CCI_INSPQ_2018_CIM10 ----------------------------------------------------
+
+ComorbidityWeights$CCI_INSPQ_2018_CIM10 <-
+  as.data.table(read_excel("inst/extdata/Comorbidity_weights.xlsx", "CCI_INSPQ_2018_CIM10"))
+ComorbidityWeights$CCI_INSPQ_2018_CIM10[, POIDS := as.integer(POIDS)]
+setkey(ComorbidityWeights$CCI_INSPQ_2018_CIM10, DIAGN_CODE)
+
+# UManitoba_2016 ----------------------------------------------------------
+
+ComorbidityWeights$UManitoba_2016 <-
+  as.data.table(read_excel("inst/extdata/Comorbidity_weights.xlsx", "UManitoba_2016"))
+ComorbidityWeights$UManitoba_2016[, POIDS := as.integer(POIDS)]
+setkey(ComorbidityWeights$UManitoba_2016, DIAGN_CODE)
+
+
+
+attr(ComorbidityWeights, "MaJ") <- Sys.Date()
+use_data(ComorbidityWeights, overwrite = TRUE)
