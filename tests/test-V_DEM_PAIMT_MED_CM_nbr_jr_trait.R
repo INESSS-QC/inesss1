678b2d7e5aeb6441f9d785b0a78595dd945175f3
Guillaume Boucher <guiboucher8@gmail.com>
2021-05-16 10:09:26 -0400

    Update test-V_DEM_PAIMT_MED_CM_nbr_jr_trait.R

--- tests/test-V_DEM_PAIMT_MED_CM_nbr_jr_trait.R
+++ tests/test-V_DEM_PAIMT_MED_CM_nbr_jr_trait.R
@@ -1,37 +1,12 @@
 library(inesss)
-library(data.table)
+library(writexl)
 
 conn <- SQL_connexion("ms045")
-
 t1 <- Sys.time()
-DENOM <- rmNA(sunique(V_DEM_PAIMT_MED_CM$COD_DENOM_COMNE$DENOM))
-DT <- vector("list", length(DENOM))
-i <- 1L
-for (denom in DENOM) {
-  dt <- as.data.table(odbc::dbGetQuery(
-    conn, statement = paste0(
-      "select SMED_COD_SERV_1 as SMED_COD_SERV,\n",
-      "       SMED_NBR_JR_DUREE_TRAIT as DUREE_TX\n",
-      "from V_DEM_PAIMT_MED_CM\n",
-      "where SMED_DAT_SERV between '2020-01-01' and '2020-12-31'\n",
-      "    and SMED_COD_DENOM_COMNE = '",denom,"';"
-    )
-  ))
-  if (nrow(dt)) {
-    DT[[i]] <- dt
-  }
-  i <- i + 1L
-}
-DT <- rbindlist(DT)
+dt_stats <- SQL_stats_SMED_NBR_JR_DUREE_TRAIT(
+  conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+  by_code_serv = TRUE, include_dureeTx_0 = FALSE
+)
 t2 <- Sys.time()
-difftime(t2, t1)
 
-DT <- dt[
-  , .(MIN = min(DUREE_TX),
-      MEAN = mean(DUREE_TX),
-      MEDIAN = median(DUREE_TX),
-      MAX = max(DUREE_TX)),
-  .(SMED_COD_SERV)
-]
-
-
+write_xlsx(dt_stats, "tests/stats_SMED_NBR_JR_DUREE_TRAIT.xlsx")
