083d532f9e0f39056706d19c6177f58e305915f2
Guillaume Boucher <guiboucher8@gmail.com>
2021-05-23 10:02:39 -0400

    Update test-V_DEM_PAIMT_MED_CM_nbr_jr_trait.R

--- tests/test-V_DEM_PAIMT_MED_CM_nbr_jr_trait.R
+++ tests/test-V_DEM_PAIMT_MED_CM_nbr_jr_trait.R
@@ -1,7 +1,9 @@
 library(inesss)
 library(writexl)
 
-conn <- SQL_connexion("ms045")
+conn <- SQL_connexion()
+
+
 t1 <- Sys.time()
 dt_stats <- SQL_stats_SMED_NBR_JR_DUREE_TRAIT(
   conn = conn, debut = "2020-01-01", fin = "2020-12-31",
@@ -10,3 +12,15 @@ dt_stats <- SQL_stats_SMED_NBR_JR_DUREE_TRAIT(
 t2 <- Sys.time()
 
 write_xlsx(dt_stats, "tests/stats_SMED_NBR_JR_DUREE_TRAIT.xlsx")
+
+
+
+
+t1 <- Sys.time()
+dt_stats_0 <- SQL_stats_SMED_NBR_JR_DUREE_TRAIT(
+  conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+  by_code_serv = TRUE, include_dureeTx_0 = TRUE
+)
+t2 <- Sys.time()
+
+write_xlsx(dt_stats_0, "tests/stats_SMED_NBR_JR_DUREE_TRAIT_0include.xlsx")
