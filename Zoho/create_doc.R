ce2a918ce50966ab905e5ef78acdaad226a5b92f
Guillaume Boucher <guiboucher8@gmail.com>
2021-05-26 00:51:29 -0400

    Update create_doc.R

--- Zoho/create_doc.R
+++ Zoho/create_doc.R
@@ -3,29 +3,37 @@
 
 ### AIDE-formulaire
 file.copy("Documentation/AIDE-FORMULAIRE-EXEMPLES-ARGUMENTS.xlsx",
-          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE-EXEMPLES-ARGUMENTS.xlsx")
+          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE-EXEMPLES-ARGUMENTS.xlsx",
+          overwrite = TRUE)
 file.copy("Documentation/AIDE-FORMULAIRE-EXEMPLES-RESULTATS.xlsx",
-          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE-EXEMPLES-RESULTATS.xlsx")
+          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE-EXEMPLES-RESULTATS.xlsx",
+          overwrite = TRUE)
 file.copy("Documentation/AIDE-FORMULAIRE.pdf",
-          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE.pdf")
+          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE.pdf",
+          overwrite = TRUE)
 
 ### Gabarits
 # naif_switch1
 file.copy("Documentation/Gabarits et Outils EXCEL/gabarit-naif_switch1.xlsx",
-          "Zoho/Outils - Formulaire interactif/gabarit-naif_switch1.xlsx")
+          "Zoho/Outils - Formulaire interactif/gabarit-naif_switch1.xlsx",
+          overwrite = TRUE)
 # stat_gen1
 file.copy("Documentation/Gabarits et Outils EXCEL/gabarit-stat_gen1.xlsx",
-          "Zoho/Outils - Formulaire interactif/gabarit-stat_gen1.xlsx")
+          "Zoho/Outils - Formulaire interactif/gabarit-stat_gen1.xlsx",
+          overwrite = TRUE)
 # PERSONAL.XLSB
 file.copy("Documentation/Gabarits et Outils EXCEL/PERSONAL.XLSB",
-          "Zoho/Outils - Formulaire interactif/PERSONAL.XLSB")
+          "Zoho/Outils - Formulaire interactif/PERSONAL.XLSB",
+          overwrite = TRUE)
 file.copy("Documentation/Gabarits et Outils EXCEL/PERSONAL.XLSB-repertoire.txt",
-          "Zoho/Outils - Formulaire interactif/PERSONAL.XLSB-repertoire.txt")
+          "Zoho/Outils - Formulaire interactif/PERSONAL.XLSB-repertoire.txt",
+          overwrite = TRUE)
 
 ### PDF package doc
 file.copy(paste0("Documentation/inesss_",as.character(packageVersion("inesss")),".pdf"),
           paste0("Zoho/Outils - Formulaire interactif/",
-                 "inesss_",as.character(packageVersion("inesss")),".pdf"))
+                 "inesss_",as.character(packageVersion("inesss")),".pdf"),
+          overwrite = TRUE)
 
 
 # Syntaxes et méthodes - Comorbidité - Charlson & Elixhauser ----------------------------------
@@ -33,4 +41,5 @@ file.copy(paste0("Documentation/inesss_",as.character(packageVersion("inesss")),
 ### PDF package doc
 file.copy(paste0("Documentation/inesss_",as.character(packageVersion("inesss")),".pdf"),
           paste0("Zoho/Syntaxes et méthodes - Comorbidité - Charlson & Elixhauser/",
-                 "inesss_",as.character(packageVersion("inesss")),".pdf"))
+                 "inesss_",as.character(packageVersion("inesss")),".pdf"),
+          overwrite = TRUE)
