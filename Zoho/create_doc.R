056e6178ea6457206b4e6fd1a032467df96bd6f1
Guillaume Boucher <guiboucher8@gmail.com>
2021-05-26 00:44:20 -0400

    Doc Zoho

---
+++ Zoho/create_doc.R
@@ -0,0 +1,36 @@
+
+# Outils - Formulaire interactif --------------------------------------------------------------
+
+### AIDE-formulaire
+file.copy("Documentation/AIDE-FORMULAIRE-EXEMPLES-ARGUMENTS.xlsx",
+          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE-EXEMPLES-ARGUMENTS.xlsx")
+file.copy("Documentation/AIDE-FORMULAIRE-EXEMPLES-RESULTATS.xlsx",
+          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE-EXEMPLES-RESULTATS.xlsx")
+file.copy("Documentation/AIDE-FORMULAIRE.pdf",
+          "Zoho/Outils - Formulaire interactif/AIDE-FORMULAIRE.pdf")
+
+### Gabarits
+# naif_switch1
+file.copy("Documentation/Gabarits et Outils EXCEL/gabarit-naif_switch1.xlsx",
+          "Zoho/Outils - Formulaire interactif/gabarit-naif_switch1.xlsx")
+# stat_gen1
+file.copy("Documentation/Gabarits et Outils EXCEL/gabarit-stat_gen1.xlsx",
+          "Zoho/Outils - Formulaire interactif/gabarit-stat_gen1.xlsx")
+# PERSONAL.XLSB
+file.copy("Documentation/Gabarits et Outils EXCEL/PERSONAL.XLSB",
+          "Zoho/Outils - Formulaire interactif/PERSONAL.XLSB")
+file.copy("Documentation/Gabarits et Outils EXCEL/PERSONAL.XLSB-repertoire.txt",
+          "Zoho/Outils - Formulaire interactif/PERSONAL.XLSB-repertoire.txt")
+
+### PDF package doc
+file.copy(paste0("Documentation/inesss_",as.character(packageVersion("inesss")),".pdf"),
+          paste0("Zoho/Outils - Formulaire interactif/",
+                 "inesss_",as.character(packageVersion("inesss")),".pdf"))
+
+
+# Syntaxes et méthodes - Comorbidité - Charlson & Elixhauser ----------------------------------
+
+### PDF package doc
+file.copy(paste0("Documentation/inesss_",as.character(packageVersion("inesss")),".pdf"),
+          paste0("Zoho/Syntaxes et méthodes - Comorbidité - Charlson & Elixhauser/",
+                 "inesss_",as.character(packageVersion("inesss")),".pdf"))
