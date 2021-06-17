31d1df4c4393d45a93fde7a4afe85ec03ad334e0
Guillaume Boucher <guiboucher8@gmail.com>
2021-06-16 20:42:33 -0400

    Update PDFs_unique.R

--- Documentation/PDFs_unique/PDFs_unique.R
+++ Documentation/PDFs_unique/PDFs_unique.R
@@ -9,6 +9,8 @@ for (file in files) {
   ele <- parseRd(tools::parse_Rd(file))
   render(
     input = "Documentation/PDFs_unique/PDFs_unique_model.Rmd",
+    output_file = paste0(ele$name,".pdf"),
+    output_dir = "Documentation/PDFs_unique",
     params = list(name = ele$name)
   )
 }
