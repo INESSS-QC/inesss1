5a456af9fe295fcd0a8b4238a820325685e943a4
Guillaume Boucher <guiboucher8@gmail.com>
2021-06-24 22:52:01 -0400

    Update 0 - create all.R

--- data-raw/0 - create all.R
+++ data-raw/0 - create all.R
@@ -1,24 +1,23 @@
 library(inesss)
-library(askpass)
 
 conn <- SQL_connexion()  # connexion teradata
 send_mail <- TRUE
 mail_to <- c(
-  "guillaume.boucher@inesss.qc.ca",
-  "françois-xavier.houde@inesss.qc.ca"
+  "guillaume.boucher@inesss.qc.ca"
+  # "françois-xavier.houde@inesss.qc.ca"
 )
 
 files <- c(
-  # "CIM",
-  # "Comorbidity_Dx.R",
-  # "ComorbidityWeights.R",
+  "CIM.R",
+  "Comorbidity_Dx.R",
+  "ComorbidityWeights.R",
   "DBC_TablesV.R",
   "I_APME_DEM_AUTOR_CRITR_ETEN_CM.R",
-  # "internal_datas.R",
-  # "Obstetrics_Dx.R",
-  # "Pop_QC.R",
-  # "RLS_list.R",
-  # "RLS_tab_convert.R",
+  "internal_datas.R",
+  "Obstetrics_Dx.R",
+  "Pop_QC.R",
+  "RLS_list.R",
+  "RLS_tab_convert.R",
   "V_DEM_PAIMT_MED_CM.R",
   "V_DENOM_COMNE_MED.R",
   "V_DES_COD.R",
