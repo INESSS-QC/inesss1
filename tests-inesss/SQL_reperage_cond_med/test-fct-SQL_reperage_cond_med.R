e147ea2cac371646334a1bb0f4494e16ffd216e8
Guillaume Boucher <guiboucher8@gmail.com>
2021-11-28 13:30:59 -0500

    doc SQL_reperage_cond_med

--- tests-inesss/SQL_reperage_cond_med/test-fct-SQL_reperage_cond_med.R
+++ tests-inesss/SQL_reperage_cond_med/test-fct-SQL_reperage_cond_med.R
@@ -2,12 +2,12 @@ test_reperage_cond_med <- SQL_reperage_cond_med(
   conn = SQL_connexion("ms045"),
   debut = "2020-06-01", fin = "2020-06-30",
   Dx_table = list(
-    diab = list(
+    diabete = list(
       CIM9 = c("2500%", "2501%", "2502%"),
       CIM10 = c("E100%", "E101%", "E109%", "E110%", "E111%", "E119%", "E130%",
                 "E131%", "E139%", "E140%", "E141%", "E149%")
     ),
-    diab_complication = list(
+    diabete_complication = list(
       CIM9 = paste0(2503:2509, "%"),
       CIM10 = paste0("E", c(102:108, 112:118, 132:138, 142:148), "%")
     )
