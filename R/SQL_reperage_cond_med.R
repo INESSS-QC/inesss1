7c49d6f8f0072539f8159d136516f46a665fd445
Guillaume Boucher <guiboucher8@gmail.com>
2022-06-05 16:34:58 -0400

    update SQL_reperage_cond_med
    Possible de déterminer le nombre de Dx qui doivent confirmer la 1re date.

--- R/SQL_reperage_cond_med.R
+++ R/SQL_reperage_cond_med.R
@@ -6,8 +6,9 @@
 #' Supposons `Dx_table = list(Angoisse = [...], Trouble = [...], Deficience = [...])`. Si `TRUE`, il y aura la colonne `DIAGN` qui indiquera des dates pour chaque élément, soit *Angoisse*, *Trouble* et *Deficience*. Si `FALSE`, la colonne `DIAGN` est absente et l'algorithme est appliqué sur tous les codes de chaque élément. Cela reviendrait à écrire tous les codes sur une même ligne.
 #'
 #' @inheritParams SQL_diagn
-#' @inheritParams confirm_nDx
+#' @inheritParams confirm_Dx_par_nDx
 #' @param by_Dx `TRUE` ou `FALSE`. Distinction entre les diagnostics (`TRUE`) ou pas (`FALSE`). Si `TRUE`, on considère chaque élément de `Dx_table` où chaque élément peut contenir plusieurs codes. Le nombre d'éléments sera donc le nombre maximal d'observations (lignes résultats) par individu.\cr Si `FALSE`, on considère tous les éléments de `Dx_Table` comme un seul, on aura donc au maximum une ligne résultat par individu.\cr Voir Détails.
+#' @param nDx Nombre de diagnostics qui doivent confirmer la 1re date. Par défaut 1.
 #'
 #' @return `data.table` :
 #' * `ID` : Identifiant de l'individu.
@@ -20,65 +21,155 @@
 #' @encoding UTF-8
 #' @import data.table
 #' @export
+#' @examples
+#' EX1_default <- SQL_reperage_cond_med(
+#'   conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+#'   Dx_table = list(
+#'     diabete = list(
+#'       CIM9 = c("2500%", "2501%", "2502%"),
+#'       CIM10 = c("E100%", "E101%", "E109%", "E110%", "E111%", "E119%", "E130%",
+#'                 "E131%", "E139%", "E140%", "E141%", "E149%")
+#'     ),
+#'     diabete_complication = list(
+#'       CIM9 = paste0(2503:2509, "%"),
+#'       CIM10 = paste0("E", c(102:108, 112:118, 132:138, 142:148), "%")
+#'     )
+#'   ),
+#'   CIM = c("CIM9", "CIM10"), by_Dx = TRUE, date_dx_var = "admis",
+#'   n1 = 30, n2 = 730,
+#'   nDx = 1
+#' )
+#' EX2_nDx0 <- SQL_reperage_cond_med(
+#'   conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+#'   Dx_table = list(
+#'     diabete = list(
+#'       CIM9 = c("2500%", "2501%", "2502%"),
+#'       CIM10 = c("E100%", "E101%", "E109%", "E110%", "E111%", "E119%", "E130%",
+#'                 "E131%", "E139%", "E140%", "E141%", "E149%")
+#'     ),
+#'     diabete_complication = list(
+#'       CIM9 = paste0(2503:2509, "%"),
+#'       CIM10 = paste0("E", c(102:108, 112:118, 132:138, 142:148), "%")
+#'     )
+#'   ),
+#'   nDx = 0
+#' )
+#' EX3_nDx2 <- SQL_reperage_cond_med(
+#'   conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+#'   Dx_table = list(
+#'     diabete = list(
+#'       CIM9 = c("2500%", "2501%", "2502%"),
+#'       CIM10 = c("E100%", "E101%", "E109%", "E110%", "E111%", "E119%", "E130%",
+#'                 "E131%", "E139%", "E140%", "E141%", "E149%")
+#'     ),
+#'     diabete_complication = list(
+#'       CIM9 = paste0(2503:2509, "%"),
+#'       CIM10 = paste0("E", c(102:108, 112:118, 132:138, 142:148), "%")
+#'     )
+#'   ),
+#'   nDx = 2
+#' )
+#' EX4_byDxFALSE_nDx0 <- SQL_reperage_cond_med(
+#'   conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+#'   Dx_table = list(
+#'     diabete = list(
+#'       CIM9 = c("2500%", "2501%", "2502%"),
+#'       CIM10 = c("E100%", "E101%", "E109%", "E110%", "E111%", "E119%", "E130%",
+#'                 "E131%", "E139%", "E140%", "E141%", "E149%")
+#'     ),
+#'     diabete_complication = list(
+#'       CIM9 = paste0(2503:2509, "%"),
+#'       CIM10 = paste0("E", c(102:108, 112:118, 132:138, 142:148), "%")
+#'     )
+#'   ),
+#'   by_Dx = FALSE, nDx = 0
+#' )
+#' EX5_byDxFALSE_nDx2 <- SQL_reperage_cond_med(
+#'   conn = conn, debut = "2020-01-01", fin = "2020-12-31",
+#'   Dx_table = list(
+#'     diabete = list(
+#'       CIM9 = c("2500%", "2501%", "2502%"),
+#'       CIM10 = c("E100%", "E101%", "E109%", "E110%", "E111%", "E119%", "E130%",
+#'                 "E131%", "E139%", "E140%", "E141%", "E149%")
+#'     ),
+#'     diabete_complication = list(
+#'       CIM9 = paste0(2503:2509, "%"),
+#'       CIM10 = paste0("E", c(102:108, 112:118, 132:138, 142:148), "%")
+#'     )
+#'   ),
+#'   by_Dx = FALSE, nDx = 2
+#' )
 SQL_reperage_cond_med <- function(
   conn = SQL_connexion(),
   debut, fin,
   Dx_table,
   CIM = c("CIM9", "CIM10"),
+  nDx = 1,
   by_Dx = TRUE,
   date_dx_var = "admis",
   n1 = 30, n2 = 730,
   ...
-  # keep_all = FALSE,
-  # verbose = TRUE
 ) {
 
   ### Arguments qui sont peut-etre dans <...> : les creer si ce n'est pas le cas
-  if (missing(keep_all)) {
+  dotargs <- list(...)
+  if (!any(names(dotargs) == "keep_all")) {
     keep_all <- FALSE
   }
-  if (missing(verbose)) {
+  if (!any(names(dotargs) == "verbose")) {
     verbose <- TRUE
   }
 
   ### Algorithme de repérage d'une condition médicale - étape 1 - MEDECHO
   # Extraction de tous les Dx selon les critères de l'étape 1
-  if (verbose) {
-    cat("Étape 1 :\n")  # indiquer que l'étape 1 commence
-  }
-  Dx_etape1 <- SQL_diagn(
-    conn = conn, cohort = NULL,
-    debut = debut, fin = fin,
-    Dx_table = Dx_table, CIM = CIM,
-    dt_source = c("V_DIAGN_SEJ_HOSP_CM", "V_SEJ_SERV_HOSP_CM"),
-    dt_desc = list(V_DIAGN_SEJ_HOSP_CM = "MEDECHO", V_SEJ_SERV_HOSP_CM = "MEDECHO"),
-    date_dx_var = date_dx_var,
-    typ_diagn = c("P", "S", "D"),
-    exclu_diagn = NULL, verbose = verbose
-  )
-  if (verbose) {
-    cat("\n")  # retour de ligne pour l'affichage de l'étape 2
-  }
-  if (!is.null(Dx_etape1)) {
-    # Conserver la dernière date - besoin d'indiquer la date la plus récente plus tard
-    if (by_Dx) {
-      Dx_etape1_last <- Dx_etape1[, .SD[.N], .(ID, DIAGN)][, .(ID, DIAGN, DATE_DX)]
-    } else {
-      Dx_etape1_last <- Dx_etape1[, .SD[.N], .(ID)][, .(ID, DATE_DX)]
+  if (nDx > 0) {
+    if (verbose) {
+      cat("Étape 1 :\n")  # indiquer que l'étape 1 commence
     }
-    # Conserver la 1re observation de chaque individu - 1er Dx
-    if (by_Dx) {
-      Dx_etape1 <- Dx_etape1[, .SD[1], .(ID, DIAGN)]
-      Dx_etape1 <- Dx_etape1[, .(ID, DIAGN, DI_Hospit = DATE_DX)]  # sélection + nommer les colonnes
-    } else {
-      Dx_etape1 <- Dx_etape1[, .SD[1], .(ID)]
-      Dx_etape1 <- Dx_etape1[, .(ID, DI_Hospit = DATE_DX)]  # sélection + nommer les colonnes
+    Dx_etape1 <- SQL_diagn(
+      conn = conn, cohort = NULL,
+      debut = debut, fin = fin,
+      Dx_table = Dx_table, CIM = CIM,
+      dt_source = c("V_DIAGN_SEJ_HOSP_CM", "V_SEJ_SERV_HOSP_CM"),
+      dt_desc = list(V_DIAGN_SEJ_HOSP_CM = "MEDECHO", V_SEJ_SERV_HOSP_CM = "MEDECHO"),
+      date_dx_var = date_dx_var,
+      typ_diagn = c("P", "S", "D"),
+      exclu_diagn = NULL, verbose = verbose
+    )
+    if (verbose) {
+      cat("\n")  # retour de ligne pour l'affichage de l'étape 2
+    }
+    if (!is.null(Dx_etape1)) {
+      # Conserver la dernière date - besoin d'indiquer la date la plus récente plus tard
+      if (by_Dx) {
+        Dx_etape1_last <- Dx_etape1[Dx_etape1[, .I[.N], .(ID, DIAGN)]$V1, .(ID, DIAGN, DATE_DX)]
+      } else {
+        Dx_etape1_last <- Dx_etape1[Dx_etape1[, .I[.N], .(ID)]$V1, .(ID, DATE_DX)]
+      }
+      # Conserver la 1re observation de chaque individu - 1er Dx
+      if (by_Dx) {
+        Dx_etape1 <- Dx_etape1[
+          Dx_etape1[, .I[1], .(ID, DIAGN)]$V1,
+          .(ID, DIAGN, DI_Hospit = DATE_DX)  # sélection + nommer les colonnes
+        ]
+      } else {
+        Dx_etape1 <- Dx_etape1[
+          Dx_etape1[, .I[1], .(ID)]$V1,
+          .(ID, DI_Hospit = DATE_DX)  # sélection + nommer les colonnes
+        ]
+      }
+    }
+  } else {
+    if (verbose) {
+      Dx_etape1 <- NULL
+      Dx_etape1_last <- NULL
+      cat("nDx=0 : Extraction des résultats (Pas de confirmation nécessaire).\n")
     }
   }
 
   ### Algorithme de repérage d'une condition médicale - étape 2
   # Extraction de tous les Dx selon les critères de l'étape 2
-  if (verbose) {
+  if (nDx > 0 && verbose) {
     cat("Étape 2 :\n")  # indiquer que l'étape 2 commence
   }
   Dx_etape2 <- SQL_diagn(
@@ -93,31 +184,63 @@ SQL_reperage_cond_med <- function(
     typ_diagn = c("A", "P", "S", "D"),
     exclu_diagn = NULL, verbose = verbose
   )
-  if (verbose) {
+  if (nDx > 0 && verbose) {
     cat("\n")
   }
   if (nrow(Dx_etape2)) {
     if (by_Dx) {
       Dx_etape2 <- unique(Dx_etape2[, .(ID, DIAGN, DATE_DX)])
-      Dx_etape2_last <- Dx_etape2[, .SD[.N], .(ID, DIAGN)][, .(ID, DIAGN, DATE_DX)]  # Conserver la dernière date - besoin d'indiquer la date la plus récente plus tard
+      Dx_etape2_last <- Dx_etape2[  # conserver la dernière date - besoin d'indiquer la date la plus récente plus tard
+        Dx_etape2[, .I[.N], .(ID, DIAGN)]$V1,
+        .(ID, DIAGN, DATE_DX)
+      ]
     } else {
       Dx_etape2 <- unique(Dx_etape2[, .(ID, DATE_DX)])
-      Dx_etape2_last <- Dx_etape2[, .SD[.N], .(ID)][, .(ID, DATE_DX)]  # Conserver la dernière date - besoin d'indiquer la date la plus récente plus tard
+      Dx_etape2_last <- Dx_etape2[  # conserver la dernière date - besoin d'indiquer la date la plus récente plus tard
+        Dx_etape2[, .I[.N], .(ID)]$V1,
+        .(ID, DATE_DX)
+      ]
     }
 
     ### Confirmation des Dx de l'étape 2
-    if (verbose) {
+    if (nDx > 0 && verbose) {
       cat("Confirmation des diagnostics...\n")
+      # Ajouter une colonne substitut si by_Dx = FALSE
+      if (!by_Dx) {
+        Dx_etape2[, DIAGN2 := 1L]
+      }
+      # Confirmation des diagnostics
+      Dx_etape2 <- confirm_Dx_par_nDx(
+        dt = Dx_etape2,
+        ID = "ID",
+        DIAGN = {if (by_Dx) "DIAGN" else "DIAGN2"},
+        DATE_DX = "DATE_DX",
+        nDx = nDx, n1 = n1, n2 = n2,
+        keep_all = FALSE
+      )
+      if (!by_Dx) {  # supprimer la colonne substitut si elle a ete cree precedemment
+        Dx_etape2[, DIAGN := NULL]
+      }
+      if (nrow(Dx_etape2)) {  # renommer les colonnes
+        setnames(Dx_etape2, c("DATE_REF", paste0("DATE_CONF", 1:nDx)), c("DI_Acte", paste0("DC_Acte", 1:nDx)))
+      }
+    } else {
+      # Si pas de confirmation, on conserve la 1re date = la plus ancienne
+      if (by_Dx) {
+        Dx_etape2 <- Dx_etape2[
+          Dx_etape2[, .I[1], .(ID, DIAGN)]$V1,
+          .(ID, DIAGN, DI_Acte = DATE_DX)
+        ]
+      } else {
+        Dx_etape2 <- Dx_etape2[
+          Dx_etape2[, .I[1], .(ID)]$V1,
+          .(ID, DI_Acte = DATE_DX)
+        ]
+      }
     }
-    Dx_etape2 <- confirm_2Dx(
-      dt = Dx_etape2, ID = "ID", DATE = "DATE_DX",
-      DIAGN = {if (by_Dx) "DIAGN" else NULL},
-      study_start = NULL, study_end = NULL,
-      n1 = n1, n2 = n2,
-      keep_first = TRUE, reverse = FALSE
-    )
-    if (nrow(Dx_etape2)) {
-      setnames(Dx_etape2, c("DATE_REP", "DATE_CONF1"), c("DI_Acte", "DC_Acte"))
+  } else {
+    if (nDx == 0) {
+      stop("Aucune données à la suite de l'extraction.")
     }
   }
 
@@ -138,9 +261,15 @@ SQL_reperage_cond_med <- function(
   if (!is.null(Dx_recent)) {
     setkey(Dx_recent)
     if (by_Dx) {
-      Dx_recent <- Dx_recent[, .SD[.N], .(ID, DIAGN)][, .(ID, DIAGN, D_Recent = DATE_DX)]
+      Dx_recent <- Dx_recent[
+        Dx_recent[, .I[.N], .(ID, DIAGN)]$V1,
+        .(ID, DIAGN, D_Recent = DATE_DX)
+      ]
     } else {
-      Dx_recent <- Dx_recent[, .SD[.N], .(ID)][, .(ID, D_Recent = DATE_DX)]
+      Dx_recent <- Dx_recent[
+        Dx_recent[, .I[.N], .(ID)]$V1,
+        .(ID, D_Recent = DATE_DX)
+      ]
     }
   }
 
@@ -165,19 +294,33 @@ SQL_reperage_cond_med <- function(
       }
     }
     # Ajouter les colonnes manquantes (si Dx_etape 1 ou 2 n'existe pas)
-    for (col in c("ID", "DIAGN", "DI_Finale", "DI_Hospit", "DI_Acte", "DC_Acte", "D_Recent")) {
-      if (!any(names(dt_final) == col)) {
-        dt_final[, (col) := NA]
+    if (nDx > 0) {
+      for (col in c("ID", "DIAGN", "DI_Finale", "DI_Hospit", "DI_Acte", paste0("DC_Acte", 1:nDx), "D_Recent")) {
+        if (!any(names(dt_final) == col)) {
+          dt_final[, (col) := NA]
+        }
       }
     }
     dt_final[, DI_Finale := DI_Acte]
-    dt_final[is.na(DI_Acte), DI_Finale := DI_Hospit]
-    dt_final[DI_Hospit < DI_Acte, DI_Finale := DI_Hospit]
-
-    if (by_Dx) {
-      setcolorder(dt_final, c("ID", "DIAGN", "DI_Finale", "DI_Hospit", "DI_Acte", "DC_Acte", "D_Recent"))
+    if (nDx > 0) {
+      dt_final[is.na(DI_Acte), DI_Finale := DI_Hospit]
+      dt_final[DI_Hospit < DI_Acte, DI_Finale := DI_Hospit]
     } else {
-      dt_final <- dt_final[, .(ID, DI_Finale, DI_Hospit, DI_Acte, DC_Acte, D_Recent)]
+      dt_final[, DI_Acte := NULL]
+    }
+
+    if (nDx > 0) {
+      if (by_Dx) {
+        setcolorder(dt_final, c("ID", "DIAGN", "DI_Finale", "DI_Hospit", "DI_Acte", paste0("DC_Acte", nDx), "D_Recent"))
+      } else {
+        setcolorder(dt_final, c("ID", "DI_Finale", "DI_Hospit", "DI_Acte", paste0("DC_Acte", 1:nDx), "D_Recent"))
+      }
+    } else {
+      if (by_Dx) {
+        setcolorder(dt_final, c("ID", "DIAGN", "DI_Finale", "D_Recent"))
+      } else {
+        setcolorder(dt_final, c("ID", "DI_Finale", "D_Recent"))
+      }
     }
 
     if (verbose) {
