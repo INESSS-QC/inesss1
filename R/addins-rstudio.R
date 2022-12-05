7c569a22d0fbca637e2cf8c6d820cbf49075767e
Guillaume Boucher <guiboucher8@gmail.com>
2022-12-05 16:38:41 -0500

    update addins
    Incorporer domaine_valeurs()

--- R/addins-rstudio.R
+++ R/addins-rstudio.R
@@ -6,3 +6,13 @@
 formulaire_addin <- function() {
   return(inesss:::formulaire())
 }
+
+
+#' Addin
+#'
+#' @return \link{domaine_valeurs}
+#' @keywords internal
+#' @encoding UTF-8
+domaine_valeurs_addin <- function() {
+  return(inesss:::domaine_valeurs())
+}
