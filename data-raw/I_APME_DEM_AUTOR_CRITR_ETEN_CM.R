f7b1e3e35cbdb3c74c80e732140e52bf73750e4e
Guillaume Boucher <guiboucher8@gmail.com>
2023-01-14 09:28:46 -0500

    update data

--- data-raw/I_APME_DEM_AUTOR_CRITR_ETEN_CM.R
+++ data-raw/I_APME_DEM_AUTOR_CRITR_ETEN_CM.R
@@ -4,6 +4,7 @@ library(data.table)
 library(askpass)
 library(inesss)
 library(stringr)
+library(lubridate)
 color_text <- function(x) {return(crayon::italic(crayon::green(x)))}
 conn <- SQL_connexion(user, pwd)
 
@@ -43,20 +44,53 @@ des_court_indcn_recnu <- function() {
         "select distinct\n",
         "    APME_COD_DENOM_COMNE_DEM as DENOM_DEM,\n",
         "    APME_COD_DIN_DEM as DIN_DEM,\n",
+        "    NPME_DES_COURT_INDCN_RECNU as DES_COURT_INDCN_RECNU,\n",
         "    extract(year from APME_DAT_STA_DEM_PME) as ANNEE,\n",
-        "    extract(month from APME_DAT_STA_DEM_PME) as MOIS,\n",
-        "    NPME_DES_COURT_INDCN_RECNU as DES_COURT_INDCN_RECNU\n",
+        "    extract(month from APME_DAT_STA_DEM_PME) as MOIS\n",
         "from PROD.I_APME_DEM_AUTOR_CRITR_ETEN_CM\n",
         "where APME_DAT_STA_DEM_PME between '",date_ymd(yr, mth, 1),"' and '",date_ymd(yr, mth, "last"),"'\n",
         "    and NPME_DES_COURT_INDCN_RECNU is not null\n",
-        ";"
+        "order by DENOM_DEM, DIN_DEM, DES_COURT_INDCN_RECNU, ANNEE, MOIS;"
       )))
       i <- i + 1L
     }
   }
   DT <- rbindlist(DT)
+  DT[
+    , ANNEEMOIS := fcase(
+      MOIS < 10, as.integer(paste0(ANNEE, "0", MOIS)),
+      MOIS >= 10, as.integer(paste0(ANNEE, MOIS))
+    )
+  ]
+  DT[
+    , diff := ANNEEMOIS - shift(ANNEEMOIS),
+    .(DENOM_DEM, DIN_DEM, DES_COURT_INDCN_RECNU)
+  ]
+  DT[is.na(diff), diff := 0L]  # 1er d'un groupe, inscrire 0
+  DT[diff == 89, diff := 1L]  # 89 = Janvier - Décembre année précédente
+  DT[, per := fcase(diff > 1, 1L, default = 0L)]
+  DT[, per := cumsum(per) + 1L, .(DENOM_DEM, DIN_DEM, DES_COURT_INDCN_RECNU)]
+  DT <- DT[
+    , .(DebPeriodeDesc = min(ANNEEMOIS),
+        FinPeriodeDesc = max(ANNEEMOIS)),
+    keyby = .(DENOM_DEM, DIN_DEM, DES_COURT_INDCN_RECNU, per)
+  ][, per := NULL]
+  DT[
+    , DebPeriodeDesc := as_date(paste0(
+      str_sub(DebPeriodeDesc, 1, 4), "-",
+      str_pad(str_sub(DebPeriodeDesc, 5, 6), width = 2, side = "left", pad = "0"), "-",
+      "01"
+    ))
+  ]
+  DT[
+    , FinPeriodeDesc := as_date(paste0(
+      str_sub(FinPeriodeDesc, 1, 4), "-",
+      str_pad(str_sub(FinPeriodeDesc, 5, 6), width = 2, side = "left", pad = "0"), "-",
+      "01"
+    ))
+  ][, FinPeriodeDesc := FinPeriodeDesc %m+% months(1, FALSE) - 1L]
 
-  setkey(DT, DENOM_DEM, DIN_DEM, ANNEE, MOIS)
+  setkey(DT, DENOM_DEM, DIN_DEM, DES_COURT_INDCN_RECNU)
   attr(DT, "verif_loop_var") <- verif_loop_var
   attr(DT, "name_loop_var") <- name_loop_var
   return(DT)
