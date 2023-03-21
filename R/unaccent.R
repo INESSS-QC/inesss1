17bc0f3e3ae6981b627038892dc7ea97b9c3694e
Guillaume Boucher <guiboucher8@gmail.com>
2023-03-20 21:44:43 -0400

    create unaccent

---
+++ R/unaccent.R
@@ -0,0 +1,18 @@
+#' Supprimer les accents
+#'
+#' Permet de convertir des caractères avec accents sans accent.
+#'
+#' @param text Chaîne de caractères à convertir, où l'on veut supprimer les accents.
+#'
+#' @return \code{text} sans accent.
+#' @encoding UTF-8
+#' @importFrom stringr str_replace_all
+#' @export
+#' @examples
+#' unaccent("âàéèïì")
+unaccent <- function(text) {
+  text <- str_replace_all(text, "['`^~\"]", " ")
+  text <- iconv(text, to = "ASCII//TRANSLIT//IGNORE")
+  text <- str_replace_all(text, "['`^~\"]", "")
+  return(text)
+}
