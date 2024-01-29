f7eb983cc42b01d1f56d3d318cbf8ed421545d62
Guillaume Boucher <guiboucher8@gmail.com>
2024-01-28 23:16:07 -0500

    Update build_package.R
    Supprime le fichier .Rhistory laisser par d'autres utilisateurs

--- DER.inesss/build_package.R
+++ DER.inesss/build_package.R
@@ -2,3 +2,8 @@
 if (file.exists("J:/GRP/A/5/A/Commun/0 Outils/Librairies R/fileExist")) {
   devtools::build(path = "J:/GRP/A/5/A/Commun/0 Outils/Librairies R/DER.inesss.tar.gz")
 }
+
+# Supprimer le fichier .Rhistory s'il existe
+if (file.exists("J:/GRP/A/5/A/Commun/0 Outils/Librairies R/.Rhistory")) {
+  file.remove("J:/GRP/A/5/A/Commun/0 Outils/Librairies R/.Rhistory")
+}
