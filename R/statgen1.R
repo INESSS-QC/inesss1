4258cf61eabc5a9001a485deb34ad47ecca307a0
Guillaume Boucher <guiboucher8@gmail.com>
2024-02-13 22:32:30 -0500

    dev statgen1

--- R/statgen1.R
+++ R/statgen1.R
@@ -74,6 +74,13 @@ statgen1 <- function(
         ))
       }
     },
+    with.temp.select.periode_ref = function() {
+      return(paste0(
+        indent(2),"case when (extract month from SMED_DAT_SERV) >= (extract month from DAT.MIN_DATE) then SMED_AN_CIVIL_DAT + 1\n",
+        indent(2),"     else SMED_AN_CIVIL_DAT\n",
+        indent(2),"     end as PERIODE_REF"
+      ))
+    },
     with.temp.from = function(typeRx, cat_list_med) {
       from <- paste0(indent(),"from V_DEM_PAIMT_MED_CM, DAT, ",typeRx)
       if (!is.null(cat_list_med)) {
@@ -89,13 +96,52 @@ statgen1 <- function(
         indent(2),"and ",varname," in ",typeRx,".",typeRx,"\n"
       ))
     },
+    with.temp.where.cod_serv = function(code_serv, code_serv_filtre) {
+      if (code_serv_filtre == "Exclusion") {
+        return(paste0(
+          indent(2),"and (SMED_COD_SERV_1 not in (",qu(code_serv),") or SMED_COD_SERV_1 is null)\n"
+        ))
+      } else if (code_serv_filtre == "Inclusion"){
+        return(paste0(
+          indent(2),"and SMED_COD_SERV_1 in (",qu(code_serv),")\n"
+        ))
+      }
+    },
     with.temp.where.cat_liste_med = function(cat_list_med) {
       if (is.null(cat_list_med)) {
         return(NULL)
       } else {
         return(paste0(indent(2),"and SMED_COD_CATG_LISTE_MED in CAT.LISTE\n"))
       }
+    },
+    with.temp.groupby = function(cat_list_med) {
+      grpby <- vector("character", 7)
+      grpby[1:4] <- c("GROUPE_AGE", "PERIODE_REF", "ANNEE_CIVILE", "MOIS")
+      grpby[7] <- "SMED_COD_TENR_MED"
+      if (!is.null(cat_list_med)) {
+        grpby[5] <- "SMED_COD_CATG_LISTE_MED"
+      }
+      if (typeRx == "DENOM") {
+        grpby[6] <- "SMED_COD_DENOM_COMNE"
+      } else if (typeRx == "DIN") {
+        grpby[6] <- "SMED_COD_DIN"
+      }
+
+      return(paste(grpby[grpby != ""], collapse = ", "))
     }
+  )
+  statgen1_functions <- c(statgen1_functions, list(
+    with.ben.select.groupe_age = statgen1_functions$with.temp.select.groupe_age,
+    with.ben.select.periode_ref = statgen1_functions$with.temp.select.periode_ref,
+    with.ben.from = statgen1_functions$with.temp.from,
+    with.ben.where.typeRx = statgen1_functions$with.temp.where.typeRx,
+    with.ben.where.cod_serv = statgen1_functions$with.temp.where.cod_serv,
+    with.ben.where.cat_liste_med =  statgen1_functions$with.temp.where.cat_liste_med,
+  ))
+
+  query <- paste0(
+    statgen1_functions$with.date(debut),
+    statgen1_functions$with.codesRx(typeRx, codesRx)
   )
 
 }
